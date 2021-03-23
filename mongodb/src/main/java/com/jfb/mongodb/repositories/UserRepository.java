520da2b669ba7408bd2da17471c6badf175bbd29
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-03-23 00:35:59 -0300

    Inserting Users

---
+++ mongodb/src/main/java/com/jfb/mongodb/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jfb.mongodb.repositories;
+
+import com.jfb.mongodb.models.entities.User;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
