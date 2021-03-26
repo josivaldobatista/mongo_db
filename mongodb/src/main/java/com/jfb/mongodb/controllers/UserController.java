2aa614417ff27f2d29c9bdce3defd3365602c2d7
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-03-25 22:12:48 -0300

    POST user

--- mongodb/src/main/java/com/jfb/mongodb/controllers/UserController.java
+++ mongodb/src/main/java/com/jfb/mongodb/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.jfb.mongodb.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import com.jfb.mongodb.models.DTO.UserDTO;
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
@@ -29,5 +33,13 @@ public class UserController {
   public ResponseEntity<UserDTO> findById(@PathVariable("id") String id) {
     UserDTO obj = service.findById(id);
     return ResponseEntity.ok().body(obj);
+  }
+
+  @PostMapping
+  public ResponseEntity<UserDTO> insert(@RequestBody UserDTO obj) {
+    obj = service.insert(obj);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+      .buildAndExpand(obj.getId()).toUri();
+    return ResponseEntity.created(uri).body(obj);
   }
 }
