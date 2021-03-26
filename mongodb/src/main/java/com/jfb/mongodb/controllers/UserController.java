94e55ae0c12761422508a62a8e6d1d378a2a9ccb
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-03-25 21:47:07 -0300

    GET user by id

--- mongodb/src/main/java/com/jfb/mongodb/controllers/UserController.java
+++ mongodb/src/main/java/com/jfb/mongodb/controllers/UserController.java
@@ -8,6 +8,7 @@ import com.jfb.mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class UserController {
   public ResponseEntity<List<UserDTO>> findAll() {
     List<UserDTO> dto = service.findAll();
     return ResponseEntity.ok().body(dto);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<UserDTO> findById(@PathVariable("id") String id) {
+    UserDTO obj = service.findById(id);
+    return ResponseEntity.ok().body(obj);
   }
 }
--- mongodb/src/main/java/com/jfb/mongodb/services/UserService.java
+++ mongodb/src/main/java/com/jfb/mongodb/services/UserService.java
@@ -1,11 +1,13 @@
 package com.jfb.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.jfb.mongodb.models.DTO.UserDTO;
 import com.jfb.mongodb.models.entities.User;
 import com.jfb.mongodb.repositories.UserRepository;
+import com.jfb.mongodb.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +21,11 @@ public class UserService {
 	public List<UserDTO> findAll() {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+
+	public UserDTO findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Objecto não encontrado"));
+		return new UserDTO(entity);
 	}
 }
