5c301c36e90a13b1b28a174b01d2919c8ab96724
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-03-27 06:53:31 -0300

    Exception Handling

---
+++ mongodb/src/main/java/com/jfb/mongodb/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.jfb.mongodb.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.jfb.mongodb.services.exceptions.ResourceNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+    HttpStatus status = HttpStatus.NOT_FOUND;
+
+    StandardError error = new StandardError();
+    error.setError("Not found");
+    error.setMessage(e.getMessage());
+    error.setPath(request.getRequestURI());
+    error.setStatus(status.value());
+    error.setTimestamp(Instant.now());
+
+    return ResponseEntity.status(status).body(error);
+  }
+}
