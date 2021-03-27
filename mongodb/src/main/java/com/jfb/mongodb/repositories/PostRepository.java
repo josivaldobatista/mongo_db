245ea2a9ac028bda9916952632515a923340e98b
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-03-27 07:49:28 -0300

    GET posts by title query operators

--- mongodb/src/main/java/com/jfb/mongodb/repositories/PostRepository.java
+++ mongodb/src/main/java/com/jfb/mongodb/repositories/PostRepository.java
@@ -5,10 +5,14 @@ import java.util.List;
 import com.jfb.mongodb.models.entities.Post;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+  List<Post> searchTitle(String text);
 
   List<Post> findByTitleContainingIgnoreCase(String text);
 }
